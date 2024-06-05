28ac6960a207c48994218e5da9b370a353551d3e
j <j@DESKTOP-1C7RB42>
2024-06-05 11:02:07 +0900

    초기 커밋.

---
+++ KU-2024-CGIP-HW5-2.cpp
@@ -0,0 +1,286 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <cmath>
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include <float.h>
+
+#include "glew-2.1.0/include/GL/glew.h"
+#include "freeglut/include/GL/glut.h"
+
+
+struct Vector3
+{
+	float			x, y, z;
+};
+
+struct Triangle
+{
+	unsigned int 	indices[3];
+};
+
+std::vector<Vector3>	gPositions;
+std::vector<Vector3>	gNormals;
+std::vector<Triangle>	gTriangles;
+
+void tokenize(char* string, std::vector<std::string>& tokens, const char* delimiter)
+{
+	char* token = strtok(string, delimiter);
+	while (token != NULL)
+	{
+		tokens.push_back(std::string(token));
+		token = strtok(NULL, delimiter);
+	}
+}
+
+int face_index(const char* string)
+{
+	int length = strlen(string);
+	char* copy = new char[length + 1];
+	memset(copy, 0, length + 1);
+	strcpy(copy, string);
+
+	std::vector<std::string> tokens;
+	tokenize(copy, tokens, "/");
+	delete[] copy;
+	if (tokens.front().length() > 0 && tokens.back().length() > 0 && atoi(tokens.front().c_str()) == atoi(tokens.back().c_str()))
+	{
+		return atoi(tokens.front().c_str());
+	}
+	else
+	{
+		printf("ERROR: Bad face specifier!\n");
+		exit(0);
+	}
+}
+
+void load_mesh(std::string fileName)
+{
+	std::ifstream fin(fileName.c_str());
+	if (!fin.is_open())
+	{
+		printf("ERROR: Unable to load mesh from %s!\n", fileName.c_str());
+		exit(0);
+	}
+
+	float xmin = FLT_MAX;
+	float xmax = -FLT_MAX;
+	float ymin = FLT_MAX;
+	float ymax = -FLT_MAX;
+	float zmin = FLT_MAX;
+	float zmax = -FLT_MAX;
+
+	while (true)
+	{
+		char line[1024] = { 0 };
+		fin.getline(line, 1024);
+
+		if (fin.eof())
+			break;
+
+		if (strlen(line) <= 1)
+			continue;
+
+		std::vector<std::string> tokens;
+		tokenize(line, tokens, " ");
+
+		if (tokens[0] == "v")
+		{
+			float x = atof(tokens[1].c_str());
+			float y = atof(tokens[2].c_str());
+			float z = atof(tokens[3].c_str());
+
+			xmin = std::min(x, xmin);
+			xmax = std::max(x, xmax);
+			ymin = std::min(y, ymin);
+			ymax = std::max(y, ymax);
+			zmin = std::min(z, zmin);
+			zmax = std::max(z, zmax);
+
+			Vector3 position = { x, y, z };
+			gPositions.push_back(position);
+		}
+		else if (tokens[0] == "vn")
+		{
+			float x = atof(tokens[1].c_str());
+			float y = atof(tokens[2].c_str());
+			float z = atof(tokens[3].c_str());
+			Vector3 normal = { x, y, z };
+			gNormals.push_back(normal);
+		}
+		else if (tokens[0] == "f")
+		{
+			unsigned int a = face_index(tokens[1].c_str());
+			unsigned int b = face_index(tokens[2].c_str());
+			unsigned int c = face_index(tokens[3].c_str());
+			Triangle triangle;
+			triangle.indices[0] = a - 1;
+			triangle.indices[1] = b - 1;
+			triangle.indices[2] = c - 1;
+			gTriangles.push_back(triangle);
+		}
+	}
+
+	fin.close();
+
+	printf("Loaded mesh from %s. (%lu vertices, %lu normals, %lu triangles)\n", fileName.c_str(), gPositions.size(), gNormals.size(), gTriangles.size());
+	printf("Mesh bounding box is: (%0.4f, %0.4f, %0.4f) to (%0.4f, %0.4f, %0.4f)\n", xmin, ymin, zmin, xmax, ymax, zmax);
+}
+
+float  					gTotalTimeElapsed = 0;
+int 					gTotalFrames = 0;
+GLuint 					gTimer;
+
+void init_timer() {
+	glGenQueries(1, &gTimer);
+}
+
+void start_timing() {
+	glBeginQuery(GL_TIME_ELAPSED, gTimer);
+}
+
+float stop_timing() {
+	glEndQuery(GL_TIME_ELAPSED);
+
+	GLint available = GL_FALSE;
+	while (available == GL_FALSE)
+		glGetQueryObjectiv(gTimer, GL_QUERY_RESULT_AVAILABLE, &available);
+
+	GLint result;
+	glGetQueryObjectiv(gTimer, GL_QUERY_RESULT, &result);
+
+	float timeElapsed = result / (1000.0f * 1000.0f * 1000.0f);
+	return timeElapsed;
+}
+
+
+int gWidth = 512;
+int gHeight = 512;
+
+GLuint VAO, VBO_positions, VBO_normals, EBO;
+
+void init_buffers() {
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO_positions);
+	glGenBuffers(1, &VBO_normals);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_positions);
+	glBufferData(GL_ARRAY_BUFFER, gPositions.size() * sizeof(Vector3), gPositions.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_normals);
+	glBufferData(GL_ARRAY_BUFFER, gNormals.size() * sizeof(Vector3), gNormals.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), (void*)0);
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gTriangles.size() * sizeof(Triangle), gTriangles.data(), GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+}
+
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 1000.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f);
+
+	glTranslatef(0.1f, -1.0f, -1.5f);
+	glScalef(10.0f, 10.0f, 10.0f);
+
+	start_timing();
+
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, gTriangles.size() * 3, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	float timeElapsed = stop_timing();
+	gTotalFrames++;
+	gTotalTimeElapsed += timeElapsed;
+	float fps = gTotalFrames / gTotalTimeElapsed;
+	char string[1024] = { 0 };
+	sprintf(string, "OpenGL Bunny: %0.2f FPS", fps);
+	glutSetWindowTitle(string);
+
+	glutPostRedisplay();
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(gWidth, gHeight);
+	glutInitWindowPosition(100, 100);
+
+	glutCreateWindow("HW5 Vertex Arrays");
+
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		return -1;
+	}
+
+	init_timer();
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_NORMALIZE);
+
+	float ka[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	float kd[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	float ks[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float p = 0.0f;
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ka);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, kd);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, ks);
+	glMaterialf(GL_FRONT, GL_SHININESS, p);
+
+	float ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+
+	float lightDirection[] = { -1.0f, -1.0f, -1.0f, 0.0 };
+	float lightAmbient[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	float lightDiffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	float lightSpecular[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, lightDirection);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
+
+	load_mesh("bunny.obj");
+
+	init_buffers();
+
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+
+	return 0;
+}
